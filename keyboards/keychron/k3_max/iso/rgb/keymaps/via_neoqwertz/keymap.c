f0315c6992a95c1b48d35037f29814b6e74c5074
ByteSturm <mrstormie+github@gmail.com>
2025-07-05 15:06:38 +0200

    fix: Wrong mapping on layer 1: y and z, ` and ´ switched

--- keyboards/keychron/k3_max/iso/rgb/keymaps/via_neoqwertz/keymap.c
+++ keyboards/keychron/k3_max/iso/rgb/keymaps/via_neoqwertz/keymap.c
@@ -50,23 +50,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /**
  * Starting NeoQWERTZ Layers here
  */
-
 [NEO_1] = LAYOUT_iso_85(
 //   ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
 //   │esc    │ F1    │ F2    │ F3    │ F4    │ F5    │ F6    │ F7    │ F8    │ F9    │F10    │F11    │F12    │ScrS   │Del    │Light  │
       KC_ESC,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR,KC_DEL, RGB_MOD,
 //   ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┴───────┼───────┤
-//   │ ^     │ 1     │ 2     │ 3     │ 4     │ 5     │ 6     │ 7     │ 8     │ 9     │ 0     │ ß     │ ´     │         ⌫    │ pg↑   │
+//   │ ^     │ 1     │ 2     │ 3     │ 4     │ 5     │ 6     │ 7     │ 8     │ 9     │ 0     │ ß     │ `     │         ⌫    │ pg↑   │
       DE_CIRC, DE_1,   DE_2,   DE_3,   DE_4,   DE_5,   DE_6,   DE_7,   DE_8,   DE_9,   DE_0,   DE_SS, S(DE_ACUT),  KC_BSPC,   KC_PGUP,
 //   ├───────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬─────┴─┬────┴──┬────┴──┬─────┴─┬────┴──┬────┴──┬────────────┼───────┤
-//   │ tab      │ q     │ w     │ e     │ r     │ t     │ z     │ u     │ i     │ o     │ p     │ ü     │ +     │ enter      │ pg↓   |
-      KC_TAB,     DE_Q,   DE_W,   DE_E,   DE_R,   DE_T,   DE_Y,   DE_U,   DE_I,   DE_O,   DE_P,  DE_UDIA,DE_ACUT,             KC_PGDN,
+//   │ tab      │ q     │ w     │ e     │ r     │ t     │ z     │ u     │ i     │ o     │ p     │ ü     │ ´     │ enter      │ pg↓   |
+      KC_TAB,     DE_Q,   DE_W,   DE_E,   DE_R,   DE_T,   DE_Z,   DE_U,   DE_I,   DE_O,   DE_P,  DE_UDIA,DE_ACUT,             KC_PGDN,
 //   ├──────────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┐          ├───────┤
 //   │ Mod3       │ a     │ s     │ d     │ f     │ g     │ h     │ j     │ k     │ l     │ ö     │ ä     │ Mod3  │          │ home  |
       MO(NEO_3),    DE_A,   DE_S,   DE_D,   DE_F,   DE_G,   DE_H,   DE_J,   DE_K,   DE_L,  DE_ODIA,DE_ADIA,MO(NEO_3), KC_ENT, KC_HOME,
 //   ├─────────┬──┴────┬──┴────┬──┴────┬──┴────┬──┴────┬──┴────┬──┴────┬──┴────┬──┴────┬──┴────┬──┴────┬──┴───────┴──┬───────┼───────┤
 //   │⇧shf     │ Mod4  │ y     │ x     │ c     │ v     │ b     │ n     │ m     │ ,     │ .     │ -     │ ⇧shf        │ ↑     │ end   |
-      KC_LSFT, MO(NEO_4),DE_Z,   DE_X,   DE_C,   DE_V,   DE_B,   DE_N,   DE_M,  DE_COMM, DE_DOT,DE_MINS,   KC_RSFT,    KC_UP,  KC_END,
+      KC_LSFT, MO(NEO_4),DE_Y,   DE_X,   DE_C,   DE_V,   DE_B,   DE_N,   DE_M,  DE_COMM, DE_DOT,DE_MINS,   KC_RSFT,    KC_UP,  KC_END,
 //   ├─────────┼───────┴─┬─────┴───┬───┴───────┴───────┴───────┴───────┴───────┴─────┬─┴─────┬─┴─────┬─┴─────┬───────┼───────┼───────┤
 //   │ctrl     │ win     │ alt     │                      space                      │ Mod4  │   fn  │ctrl   │ ←     │ ↓     │ →     │
       KC_LCTL,   KC_LGUI,  KC_LALT,                      KC_SPC,                 MO(NEO_4),MO(WIN_FN),KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT),
